4c7a907d7a66c0b2a0c03af0e2df1dd166ddd217
DiegoProtec <diegoprotec@hotmail.com>
2015-07-10 15:33:42 -0300

    config gulp-sass

---
+++ producao/libdi/Gulpfile.js
@@ -0,0 +1,13 @@
+var path = require('path');  
+var gulp = require('gulp');  
+var gutil = require('gulp-util');  
+var clean = require('gulp-clean');  
+var concat = require('gulp-concat');  
+var uglify = require('gulp-uglify');  
+var rename = require('gulp-rename');  
+var filesize = require('gulp-filesize');  
+var changed = require('gulp-changed');  
+var watch = require('gulp-watch');
+var less = require('gulp-sass');  
+var less = require('gulp-sourcemaps');  
+
